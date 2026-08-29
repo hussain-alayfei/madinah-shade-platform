4246ab6887f69dcee0ac7fc3fefb3384cdbdb38d
HUSSAIN <65435678+hussain-alayfei@users.noreply.github.com>
2026-08-29 22:12:11 +0300

    feat: add client-only city map wrapper

---
+++ components/CityMapView.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const CityMapClient = dynamic(() => import("./CityMapClient").then((mod) => mod.CityMapClient), {
+  ssr: false,
+  loading: () => <div className="map-loading">جاري تحميل بيانات المدينة…</div>,
+});
+
+export function CityMapView() {
+  return <CityMapClient />;
+}
